6def0b104d79ec2e3656357549e985db89113595
Vladimir Hernandez <vladimir.hernandez@gmail.com>
2016-04-08 17:30:04 -0400

    Unsolved problem 9, status pending

---
+++ PENDING_0009.py
@@ -0,0 +1,18 @@
+'''
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a^2 + b^2 = c^2
+
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+'''
+
+
+'''
+solution as given by wolfram alpha:
+a=200, b=375, c=425
+'''
+
+print 200*375*425
